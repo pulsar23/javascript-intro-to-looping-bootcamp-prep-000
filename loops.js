b863cc0a549877c4fa1c5fe0e627c8f3cb3f5204
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-06-17 04:42:03 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -12,4 +12,9 @@ function doWhileLoop(array){
   function maybeTrue() {
   return Math.random() >= 0.5; // Returns a random number between 0 (inclusive) and 1 (exclusive)
 }
+  do {
+    array.pop();
+    while (array.length > 0 && maybeTrue());
+  }
+  return array;
 }
